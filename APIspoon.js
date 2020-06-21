df9098e39376cf27b5e3e071000ac1a674d8a8a8
SamirIngley <samir.ingle7@gmail.com>
2020-06-21 15:42:58 -0700

    Spoon API uses guessNutrition of recipe, and displays 4 basics on page

--- APIspoon.js
+++ APIspoon.js
@@ -1,4 +1,5 @@
 
+
 $(function(){
     $('#searchButton').click(function(){
         
@@ -7,16 +8,47 @@ $(function(){
         
         console.log('start api');
         fetch("https://api.spoonacular.com/recipes/guessNutrition?apiKey=f97e16fe3d4f48b1bd29e46d6b0154b7&title="+queryStr)
-        .then(res => res.text())
+        .then(res => res.json())
         .then(data => {
             console.log(data);
-            $('#output').html(data)
+            $('#protein').html(data.protein.value)
+            $('#fat').html(data.fat.value)
+            $('#carbs').html(data.carbs.value)
+            $('#calories').html(data.calories.value)
         })
         .catch(error => console.log('ERROR'));
         console.log('end api');
 
     })
 })
+
+
+
+// // $ notation means we're using jquery to work with the html
+// $(function(){
+//     // run this function when we click the search button
+//     $('#searchButton').click(function(){
+        
+//         //grab the query from the search box
+//         const queryStr = $('#searchBox').val()
+//         console.log(queryStr)
+
+//         // start request for data from api
+//         console.log('start api get id');
+//         const api_url = "https://api.spoonacular.com/recipes/guessNutrition?apiKey=f97e16fe3d4f48b1bd29e46d6b0154b7&title="
+//         fetch(api_url+queryStr)
+//         .then(res => {
+//             console.log(res.json())
+//             $('#output').html(res)
+//         })
+//             //from video.. to get id:
+//             // const api_url = "https://api.spoonacular.com/recipes/search?apiKey=f97e16fe3d4f48b1bd29e46d6b0154b7&number=1&query="
+
+//         .catch(error => console.log('ERROR'));
+//         console.log('end api')
+
+//     })
+// })
 
 
 
