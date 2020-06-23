7de2cdf357ad16c13efb888c2aea6dbf918e4ad4
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-06-23 09:22:53 -0700

    api and home connected

--- templates/script.js
+++ templates/script.js
@@ -1,3 +1,4 @@
+//Pop up
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
