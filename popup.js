2da1b8590c9d05eef830ff4a27a8f0ed96edc855
SamirIngley <samir.ingle7@gmail.com>
2020-07-02 19:32:52 -0700

    final structure for protein storage, not tested or connected to front end

--- popup.js
+++ popup.js
@@ -3,17 +3,27 @@
 
 // Listening for click event on Submit id: add-button
 $(function(){
+
+    // When the user opens the popup, display the current Total
+    chrome.storage.sync.get('proteinTotal', function(protein){
+        $('#proteinTotal').text(protein.proteinTotal)
+    })
+
+
+    //submit button id
     $('#add-button').click(function(){
+
+        // Get the current total from chrome storage
         chrome.storage.sync.get('proteinTotal', function(protein){
             var newProteinTotal = 0;
 
             // if a total exists, we add it to new total
-            if (protein.total){
+            if (protein.proteinTotal){
                 newProteinTotal += parseInt(protein.proteinTotal);
             }
 
             // if something was entered, update new total
-            var proteinAmount = $('#protein-amount').val;
+            var proteinAmount = $('#item-protein-amount').val;
             if (proteinAmount){
                 newProteinTotal += parseInt(proteinAmount);
             }
@@ -21,8 +31,12 @@ $(function(){
             // Set the new total in the chrome storage
             chrome.storage.sync.set({'proteinTotal':newProteinTotal})
             
-            // Update UI
-            $('#proteinTotal')
+            // Update UI - presents new total
+            $('#proteinTotal').text(newProteinTotal)
+
+
+            // if input box, clear it using below
+            // $('#item-protein-amount').val('');
         })
     })
 });
