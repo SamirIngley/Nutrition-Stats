bec1f583430570afaa36d450dc8d089989a1b1a8
SamirIngley <samir.ingle7@gmail.com>
2020-07-03 00:59:25 -0700

    EDAMAM API query works super basic

--- APIedamam.js
+++ APIedamam.js
@@ -1,2 +1,31 @@
+// DOCS: https://developer.edamam.com/edamam-docs-nutrition-api
+// FOOD TEXT ANALYSIS
 
-// "https://api.edamam.com/api/nutrition-details?app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
+// "https://api.edamam.com/api/nutrition-data?app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&ingr=1%20large%20apple"
+
+
+
+
+$(function(){
+    $('#searchButton').click(function(){
+        
+        // var queryStr = $('#searchBox').val()
+        // console.log(queryStr)
+        
+        console.log('start api');
+
+        fetch("https://api.edamam.com/api/nutrition-data?app_id=adec199b&app_key=20ffecd0c1e546e27733a30af29950f5&ingr=1%20large%20apple")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+
+        .catch(error => {
+            console.log('ERROR');
+            $('#name').html('This item does not exist.')
+        })
+
+        console.log('end api');
+
+    })
+})
