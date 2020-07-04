2a0bd17dec12a9de417620bb69b98750f61b6675
SamirIngley <samir.ingle7@gmail.com>
2020-07-04 01:30:29 -0700

    cleaned up graph, removed orange

--- popup.js
+++ popup.js
@@ -34,25 +34,23 @@ $(function(){
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Calories', 'Carbs', 'Protein', 'Fat', 'Sugar', 'Orange'],
+                labels: ['Calories', 'Carbs', 'Protein', 'Fat', 'Sugar'],
                 datasets: [{
                     label: 'Nutrient Totals',
-                    data: [caloriesValue, carbsValue, proteinValue, fatValue, sugarValue, 1],
+                    data: [caloriesValue, carbsValue, proteinValue, fatValue, sugarValue],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(153, 102, 255, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(153, 102, 255, 1)'
                     ],
                     borderWidth: 1
                 }]
@@ -276,25 +274,23 @@ $(function(){
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Calories', 'Carbs', 'Protein', 'Fat', 'Sugar', 'Orange'],
+                labels: ['Calories', 'Carbs', 'Protein', 'Fat', 'Sugar'],
                 datasets: [{
                     label: 'Nutrient Totals',
-                    data: [newCaloriesTotal, newCarbsTotal, newProteinTotal, newFatTotal, newSugarTotal, 1],
+                    data: [newCaloriesTotal, newCarbsTotal, newProteinTotal, newFatTotal, newSugarTotal],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(153, 102, 255, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(153, 102, 255, 1)'
                     ],
                     borderWidth: 1
                 }]
